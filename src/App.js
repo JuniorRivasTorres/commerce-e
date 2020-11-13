84c53f2ad80fff7e61d6874ff98807715653f7d4
Junior RivasTorres <jrtimmobilier@gmail.com>
2020-11-13 09:43:25 -0500

    checkout js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } 
+from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+//CA VA ROULER une fois quand le component telecharge 
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+
+      if (authUser) {
+  //lutilisateur login et lutilisateur log out
+      dispatch({
+        type: 'SET_USER',
+        user: authUser
+      })
+    } else {
+      dispatch({
+        type: 'SET_USER',
+        User: null
+      })
+      }
+
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login/>
+            </Route>
+            <Route path="/checkout">
+            <Header />
+              <Checkout />
+            </Route>
+            <Route path="/">
+            <Header />
+              <Home />
+          </Route>  
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal"
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+
+function Checkout() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
+    return (
+    <div className="checkout">
+        <div className="checkout__left">
+            <img 
+                className="checkout__ad"
+                src="https://images-na.ssl-images-amazon.com/images/G/15/credit/img19/CBCC/detailpage/DP-Desktop-HeaderHand_NP-Unrec_fr._CB443757328_.png" 
+                alt=""
+            />
+            <div>
+                <h3>Hello, {user?.email}</h3>
+                <h2 className="checkout__title">
+                    Your shoping Basket carte de credit niclaude
+                </h2>
+
+               
+                {basket.map(item => (
+                    <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                />
+
+                ) )}
+            {/* Checkoutproduct*/}
+            {/* BasketItem */}
+            {/* BasketItem */}
+            {/* BasketItem */}
+            {/* BasketItem */}
+            </div>
+        </div>
+        <div className="checkout__right">
+            <Subtotal />
+            <h2> Subtot </h2>
+        </div>
+      
+    </div>
+    );
+}
+
+export default Checkout;
